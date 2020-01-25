61d3fbc54725212becebdb29813c059f71223a1a
Kao <aboutkao0807@gmail.com>
2020-01-25 14:41:04 +0800

    Initial Commit

---
+++ Online Translation App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Online Translation App
+//
+//  Created by Kao on 2020/1/25.
+//  Copyright © 2020 Kao Che. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
